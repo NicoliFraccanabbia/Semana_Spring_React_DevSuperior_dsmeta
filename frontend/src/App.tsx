dfcd7296488b692f42b8bc6f7572769c3e680f04
Nicoli Fraccanabbia <nicolirama@hotmail.com>
2022-07-16 21:23:52 -0300

    Firts component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,14 @@
+import NotificationButton from "./componentes/Notificationbutton"
+
 function App() {
   return (
-    <h1> Olá Mundo!</h1>
+    <>
+      <h1> Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
