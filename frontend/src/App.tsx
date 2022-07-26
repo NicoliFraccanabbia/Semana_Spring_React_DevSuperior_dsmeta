a885adc982b652fd49252b65fb4e4fd6bfa452d2
Nicoli Fraccanabbia <nicolirama@hotmail.com>
2022-07-25 22:28:46 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,7 @@
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Header from "./componentes/Header"
 import SalesCard from "./componentes/SalesCard"
 
@@ -7,6 +10,7 @@ import SalesCard from "./componentes/SalesCard"
 function App() {
   return (
     <>
+    <ToastContainer/>
     <Header/>
     <main>
     <section id="sales">
